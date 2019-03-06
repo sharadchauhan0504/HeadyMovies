124a90c3e4d079c6932d4ae91f05f724684a796a
Sharad S. Chauhan <schauhan03@xento.com>
2019-03-06 14:43:12 +0530

    Initial Setup

---
+++ HeadyMovies/View/Controllers/MoviesHomeScreenController.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesHomeScreenController.swift
+//  HeadyMovies
+//
+//  Created by Sharad S. Chauhan on 06/03/19.
+//  Copyright © 2019 Sharad S. Chauhan. All rights reserved.
+//
+
+import UIKit
+
+class MoviesHomeScreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ HeadyMoviesTests/HeadyMoviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  HeadyMoviesTests.swift
+//  HeadyMoviesTests
+//
+//  Created by Sharad S. Chauhan on 05/03/19.
+//  Copyright © 2019 Sharad S. Chauhan. All rights reserved.
+//
+
+import XCTest
+@testable import HeadyMovies
+
+class HeadyMoviesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
